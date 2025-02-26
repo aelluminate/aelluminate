3f33709c049d10912a8453353fa031408ababe8c
Noey <ignacio.francis@aelluminate.com>
2025-02-26 11:11:09 +0800

    feat: add keywords and metadata enhancements

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,5 +1,7 @@
 import { Metadata } from "next"
 
+import { keywords } from "./keywords"
+
 const siteName = "Aelluminate"
 const siteURL = "https://aelluminate.com"
 const siteDescription = "Stop patching problem, start building solutions."
@@ -9,6 +11,7 @@ export const metadata: Metadata = {
     default: siteName,
     template: `%s | ${siteName}`,
   },
+  keywords: keywords,
   description: siteDescription,
   generator: siteName,
   applicationName: siteName,
@@ -18,10 +21,18 @@ export const metadata: Metadata = {
     url: siteURL,
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "https://aelluminate.com/og.png",
+        width: 1920,
+        height: 1080,
+        alt: "Aelluminate Open Graph Image",
+      },
+    ],
   },
   robots: {
-    index: true,
-    follow: true,
+    index: false,
+    follow: false,
     googleBot: {
       index: true,
       follow: true,
@@ -29,6 +40,22 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
+  },
+  twitter: {
+    title: siteName,
+    card: "summary_large_image",
+    site: "@aelluminate",
+    creator: "@aelluminate",
+    creatorId: "1658936245899370503",
+    description: siteDescription,
+    images: [
+      {
+        url: "https://aelluminate.com/og.png",
+        width: 1920,
+        height: 1080,
+        alt: "Aelluminate Open Graph Image",
+      },
+    ],
   },
   icons: {
     shortcut: "/favicon.ico",
